7aa7169d709067cd66c1aa64a9bf6384868cdffe
Luiz Pedro <luizpsmoreira@gmail.com>
2020-05-08 22:10:58 -0300

    add: css, js and theme support

--- header.php
+++ header.php
@@ -5,7 +5,6 @@
     <meta charset="<?php bloginfo('charset');?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0,user-scalable=no">
     <?php wp_head();?>
-    <title>Document</title>
 </head>
 
 <body <?php body_class();?>>
